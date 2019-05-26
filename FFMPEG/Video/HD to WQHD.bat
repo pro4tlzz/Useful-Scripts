7a724b81aaada4bd3ef7abac31b2ba556cf1307d
Bilal Habib <superfrillz12@hotmail.co.uk>
2019-05-26 16:50:32 +0100

    lossless conversion

--- FFMPEG/Video/HD to WQHD.bat
+++ FFMPEG/Video/HD to WQHD.bat
@@ -2,7 +2,7 @@
 
 FOR %%f IN (*.mp4) DO (
 echo Converting: %%f
-ffmpeg -i "%%f" -vf scale=2560:1440 "%%~nf.mp4"
+ffmpeg -i "%%f" -vf scale=2560:1440 -c:v libx265 -x265-params lossless=1  "%%~nf.mp4"
 )
 
 echo Finished

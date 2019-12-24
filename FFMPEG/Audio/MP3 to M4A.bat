28f5615d828f5697ece654834817b0c6c08b2b7b
Pro4TLZZ <superfrillz12@hotmail.co.uk>
2019-12-24 13:48:17 +0000

    keep cover and del mp3 files

--- FFMPEG/Audio/MP3 to M4A.bat
+++ FFMPEG/Audio/MP3 to M4A.bat
@@ -2,8 +2,9 @@
 
 FOR %%f IN (*.mp3) DO (
 echo Converting: %%f
-ffmpeg -i "%%f" -ab 320k -map_metadata 0 "%%~nf.m4a"
+ffmpeg -i "%%f" -ab 256k -c:v copy -map_metadata 0 "%%~nf.m4a"
 )
+del *.mp3
 
 echo Finished
 
